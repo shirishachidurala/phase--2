ab80daf5dceab5df9a24acf4ad1ff9b7440ef61c
shirishachidurala <97821401+shirishachidurala@users.noreply.github.com>
2022-02-07 23:24:53 +0530

    Add files via upload

--- DASHBOARD.java
+++ DASHBOARD.java
@@ -1,41 +1,51 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
 
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class DASHBOARD
- */
-@WebServlet("/DASHBOARD")
-public class DASHBOARD extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+* Servlet implementation class Dashboard
+*/
+@WebServlet("/Dashboard")
+public class Dashboard extends HttpServlet {
+        private static final long serialVersionUID = 1L;
        
     /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DASHBOARD() {
+* @see HttpServlet#HttpServlet()
+*/
+    public Dashboard() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                // TODO Auto-generated method stub
+        
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            
+            String userId = request.getParameter("userid");
+            if (userId == null ) {
+                out.println("No UserId was found in the URL.<br>");
+            } else {
+                out.println("UserId obtained from URL Rewriting:" + userId + "<br>");
+            }
+            out.println("</body></html>");
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+        }
+
+        /**
+         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                // TODO Auto-generated method stub
+                doGet(request, response);
+        }
 
 }
+
