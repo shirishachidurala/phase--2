bb13bddfba53f54d16f7e14d0be859e43df9ce9e
shirishachidurala <97821401+shirishachidurala@users.noreply.github.com>
2022-02-07 23:35:12 +0530

    Add files via upload

--- LOGINSERVLET.java
+++ LOGINSERVLET.java
@@ -1,43 +1,41 @@
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.annotation.*;
-import javax.servlet.http.*;
 
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
-* Servlet implementation class LoginServlet
-*/
-@WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
-        private static final long serialVersionUID = 1L;
+ * Servlet implementation class LOGINSERVLET
+ */
+@WebServlet("/LOGINSERVLET")
+public class LOGINSERVLET extends HttpServlet {
+	private static final long serialVersionUID = 1L;
        
     /**
-* @see HttpServlet#HttpServlet()
-*/
-    public LoginServlet() {
+     * @see HttpServlet#HttpServlet()
+     */
+    public LOGINSERVLET() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-        /**
-         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-         */
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                // TODO Auto-generated method stub
-                
-                 String userId = request.getParameter("userid");
-             response.sendRedirect("dashboard?userid=" + userId);  
-                
-        }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
 
-        /**
-         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-         */
-        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                // TODO Auto-generated method stub
-                doGet(request, response);
-        }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
-
